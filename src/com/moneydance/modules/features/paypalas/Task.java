526c3c49072d4fc908e105352e7c75851d67b4d5
Sean Reilly <sreilly@infinitekind.com>
2017-04-20 10:29:17 +0100

    copyright header update

--- src/com/moneydance/modules/features/paypalas/Task.java
+++ src/com/moneydance/modules/features/paypalas/Task.java
@@ -1,5 +1,5 @@
 /************************************************************\
- *      Copyright (C) 2008 Reilly Technologies, L.L.C.      *
+ *        Copyright 2017 The Infinite Kind, Limited         *
 \************************************************************/
 
 package com.moneydance.modules.features.paypalas;
