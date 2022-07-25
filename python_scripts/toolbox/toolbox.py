2d73bc3ad403e0c55b5c452d26fc8d230186668d
Yogi1967 <sbeesley@gmail.com>
2022-07-25 12:46:52 +0100

    Toolbox (latest preview) for 2022.5(4088)

--- python_scripts/toolbox/toolbox.py
+++ python_scripts/toolbox/toolbox.py
@@ -502,8 +502,8 @@ else:
     global MD_MDPLUS_TEST_UNIQUE_BANKING_SERVICES_BUILD
 
     TOOLBOX_MINIMUM_TESTED_MD_VERSION = 2020.0                                                                          # noqa
-    TOOLBOX_MAXIMUM_TESTED_MD_VERSION = 2022.4                                                                          # noqa
-    TOOLBOX_MAXIMUM_TESTED_MD_BUILD =   4087                                                                            # noqa
+    TOOLBOX_MAXIMUM_TESTED_MD_VERSION = 2022.5                                                                          # noqa
+    TOOLBOX_MAXIMUM_TESTED_MD_BUILD =   4088                                                                            # noqa
     MD_OFX_BANK_SETTINGS_DIR = "https://infinitekind.com/app/md/fis/"                                                   # noqa
     MD_OFX_DEFAULT_SETTINGS_FILE = "https://infinitekind.com/app/md/fi2004.dict"                                        # noqa
     MD_OFX_DEBUG_SETTINGS_FILE = "https://infinitekind.com/app/md.debug/fi2004.dict"                                    # noqa
